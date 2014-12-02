83a8324e13e00463f21cc5df672733040d2389f0
Martin Walsh <martin.walsh@gmail.com>
2014-12-02 22:14:43 +0000

    Auto 16:9 Device Scale
    Enabled ScanLine default

--- pixelart.spritebuilder/Source/MainScene.m
+++ pixelart.spritebuilder/Source/MainScene.m
@@ -6,8 +6,21 @@
 }
 
 - (void)didLoadFromCCB {
-    //[_effectNode setEffect:[[CCEffectScanline alloc] init]];
-    //[[_effectNode texture] setAntialiased:NO];
+    
+    // Design Resolution
+    CGSize designSize = CGSizeMake(400.0, 240.0);
+    
+    // Device Resolution
+    CGSize deviceSize = [CCDirector sharedDirector].viewSize;
+    
+    // Auto Scale
+    float scale  = deviceSize.width/designSize.width;
+
+    [_effectNode setScaleX:scale];
+    [_effectNode setScaleY:scale];
+    
+    [_effectNode setEffect:[[CCEffectScanline alloc] init]];
+    [[_effectNode texture] setAntialiased:NO];
 }
 
 @end

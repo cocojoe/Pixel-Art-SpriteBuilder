d5f4be9555e245d29c2f9eb2dab8bf8be798d217
Martin Walsh <martin.walsh@gmail.com>
2014-12-03 16:41:23 +0000

    TileMap Added
    Scroll Tilemap
    Touch Actions Refactored

--- pixelart.spritebuilder/Source/MainScene.m
+++ pixelart.spritebuilder/Source/MainScene.m
@@ -3,6 +3,7 @@
 
 @implementation MainScene {
     CCEffectNode* _effectNode;
+    CCTiledMap* _tileMap;
 }
 
 - (void)didLoadFromCCB {
@@ -19,8 +20,21 @@
     [_effectNode setScaleX:scale];
     [_effectNode setScaleY:scale];
     
+    // Load TMX
+    _tileMap = [CCTiledMap tiledMapWithFile:@"tilemap.tmx"];
+    [_effectNode addChild:_tileMap];
+    
     // Input
     self.userInteractionEnabled = YES;
+    
+    // Animate Map
+    CCActionMoveBy* moveActionX = [CCActionMoveBy actionWithDuration:2.0 position:ccp(designSize.width-_tileMap.contentSizeInPoints.width,0)];
+    CCActionMoveBy* moveActionY = [CCActionMoveBy actionWithDuration:2.0 position:ccp(0,designSize.height-_tileMap.contentSizeInPoints.height)];
+    
+    CCActionSequence* moveSequence = [CCActionSequence actions:moveActionX,moveActionY,[moveActionX reverse],[moveActionY reverse],nil];
+    moveSequence.tag = 1;
+    
+    [_tileMap runAction:[CCActionRepeatForever actionWithAction:moveSequence]];
     
     //[_effectNode setEffect:[[CCEffectScanline alloc] init]];
 }
@@ -28,17 +42,34 @@
 #if __CC_PLATFORM_MAC
 - (void)mouseDown:(NSEvent *)theEvent {
     CGPoint pos = [theEvent locationInNode:_effectNode]; // Relative to Design
-    [self touchAction:pos];
+    [self touchActionBegin:pos];
 }
+- (void)mouseUp:(NSEvent *)theEvent {
+    CGPoint pos = [theEvent locationInNode:_effectNode]; // Relative to Design
+    [self touchActionEnd:pos];
+}
+
 #elif __CC_PLATFORM_IOS || __CC_PLATFORM_ANDROID
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     CGPoint pos = [touch locationInNode:_effectNode]; // Relative to Design
-    [self touchAction:pos];
+    [self touchActionBegin:pos];
+}
+
+- (void)touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    CGPoint pos = [touch locationInNode:_effectNode]; // Relative to Design
+    [self touchActionEnd:pos];
 }
 #endif
 
-- (void)touchAction:(CGPoint) pos {
+- (void)touchActionBegin:(CGPoint) pos {
     CCLOG(@"Touch: %f,%f",pos.x,pos.y);
+    [self setPaused:!self.paused];
+}
+
+- (void)touchActionEnd:(CGPoint) pos {
+}
+
+- (void) update:(CCTime)delta {
 }
 
 @end
